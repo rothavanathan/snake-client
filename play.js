71117df349635b6e52961660d1e2bc4f2479565a
Romesh Thavanathan <romeshthavanathan@gmail.com>
2020-11-04 18:05:03 +0000

    separated client connection into separate module client.js

--- play.js
+++ play.js
@@ -1,20 +1,8 @@
 const net = require('net');
-
+const {connect} = (require('./client'));
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('data', (data) => {
-    console.log(data);
-  })
-  return conn;
-}
 
 console.log('Connecting ...');
 connect();
